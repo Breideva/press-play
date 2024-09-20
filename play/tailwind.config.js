e3bdec7fb2f5142935e1a2687746c585fb6333aa
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-09-20 14:57:55 -0500

    Added set styles for app

--- play/tailwind.config.js
+++ play/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#F0F5FA",
+        text: "#070C0D",
+        primary: "#D04D11",
+        secondary: "#F07942",
+        backgroundLight: "#D1E0F0",
+      },
+      fontFamily: {
+        martel: ["Martel", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
