04b97790ab976a1af21c29c31e2163910d243688
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-09-22 19:30:27 -0500

    Created header
    Created a header that includes two buttons ( that will have links ) and added two images used in the header

--- play/src/pages/Home.jsx
+++ play/src/pages/Home.jsx
@@ -1,11 +1,9 @@
-
+import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <h1 className="text-7xl font-light">hello</h1>
-      <h2 className="text-7xl font-normal">hello</h2>
-      <h3 className="text-7xl font-bold">hello</h3>
-    </div>
-  )
+    <>
+      <Header/>
+    </>
+  );
 }
