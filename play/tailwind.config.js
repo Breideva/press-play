febe493f112af141285e77d8a34c572c9e4089fb
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-09-26 15:31:34 -0500

    Added another color option

--- play/tailwind.config.js
+++ play/tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         background: "#F0F5FA",
         backgroundLight: "#E0E0E0",
         text: "#070C0D",
+        textLight: "rgba(7, 12, 13, 0.8)",
         primary: "#D1EAFF",
         secondary: "#D04D11",
         secondaryLight: "#F39468",
