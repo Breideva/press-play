240515cf4250b3de55e3608c43293b11258a53a3
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-09-26 16:59:33 -0500

    Updated header
    Added new styles to buttons so they grow when hovered over

--- play/src/components/Header.jsx
+++ play/src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import person from "../assets/body-1-person.png";
 import circles from "../assets/body-1-circles.png";
-import Recommend from "./Recommend";
+
 
 export default function Header() {
   return (
     <div className="flex justify-center ">
-      <div className="w-full fixed flex justify-center">
-        <div className="w-9/12 justify-center flex flex-col sm:flex-row gap-14 sm:gap-0 items-center mt-20">
+      <div className="w-full flex justify-center">
+        <div className="w-9/12 justify-center flex flex-col sm:flex-row gap-14 sm:gap-0 items-center mt-20 sm:mt-30 md:mt-40 mb-20 sm:mb-30 md:mb-40 ">
           <div className="relative w-3/4 sm:w-1/2">
             <img className="absolute z-20" src={person} alt="" />
             <img className="z-10 relative" src={circles} alt="" />
@@ -18,12 +18,12 @@ export default function Header() {
             </h1>
             <div className="flex items-center gap-8 sm:gap-6 md:gap-10 lg:gap-14">
               <div className="relative group flex">
-                <button className="relative text-2xl lg:text-3xl z-10 text-text bg-background p-4 rounded-lg border-b-2 border-secondaryLight transition-all duration-500 group-hover:text-background group-hover:bg-text">
+                <button className="relative text-2xl lg:text-3xl z-10 text-text bg-background p-4 rounded-lg border-b-2 border-secondaryLight transition-transform duration-700 group-hover:text-background group-hover:bg-text hover:scale-105 active:scale-95">
                   Games
                 </button>
                 <div className="bg-secondary w-full h-full absolute top-0 blur-2xl"></div>
               </div>
-              <button className="text-2xl lg:text-3xl p-3 border-4 border-secondary text-secondary shadow-xl rounded-lg transition-all duration-500 hover:border-secondaryLight hover:text-secondaryLight">
+              <button className="text-2xl lg:text-3xl p-3 border-4 border-secondary text-secondary shadow-xl rounded-lg transition-transform duration-700 hover:border-secondaryLight hover:text-secondaryLight hover:scale-105 active:scale-95">
                 Favorites
               </button>
             </div>
