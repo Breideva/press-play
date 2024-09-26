ae70c8105b02e96d8629e96057662a76bd24246c
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-09-26 17:12:07 -0500

    Added firstbody to the home page

--- play/src/pages/Home.jsx
+++ play/src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import BigBanner from "../components/BigBanner";
+import FirstBody from "../components/FirstBody";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <FirstBody />
     </>
   );
 }
