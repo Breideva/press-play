f7b187a530409c6a122ca6ae2694a129be08f8d8
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-09-29 18:52:33 -0500

    Added all components to home page

--- play/src/pages/Home.jsx
+++ play/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import BigBanner from "../components/BigBanner";
-import FirstBody from "../components/FirstBody";
+import FirstBody from "../components/Sections/FirstBody";
 import Header from "../components/Header";
+import SecondBody from "../components/Sections/SecondBody";
+import GameGrid from "../components/GameGrid";
 
 export default function Home() {
   return (
     <>
-      <Header/>
+      <Header />
       <FirstBody />
+      <SecondBody />
+      <GameGrid />
     </>
   );
 }
