7c1211a47c10c0dcba5f8f86ccb7430659b98312
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-10-03 18:29:14 -0500

    Updated styles

--- play/src/components/Sections/FirstBody.jsx
+++ play/src/components/Sections/FirstBody.jsx
@@ -1,13 +1,12 @@
-
-import BigBanner from "../BigBanner";
+import BigBanner from "../HomePage/BigBanner";
 
 export default function FirstBody() {
   return (
     <div className="flex justify-center relative bg-background text-text">
       <div className="w-full flex justify-center">
-        <div className="w-9/12 flex flex-col lg:flex-row gap-y-10 lg:gap-10 justify-center items-center pt-14 mb-20 overflow-hidden">
-          <BigBanner />    
-          <BigBanner genres="3" />    
+        <div className="w-9/12 flex flex-col lg:flex-row gap-y-10 lg:gap-8 justify-center items-center pt-14 mb-20 overflow-hidden">
+          <BigBanner />
+          <BigBanner genres="3" />
         </div>
       </div>
     </div>
