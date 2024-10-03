84bb2baa550fbef8a839da97afa37e1e3caa4a51
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-10-03 18:28:26 -0500

    Created gamedisplaytags
    These display games of certain tags. Displays the header with the correct capitalization using things like charAt, toupperCase, and slice. Also has a link to the page of the game and details

---
+++ play/src/components/GameDisplayTags.jsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { LuBookmarkPlus } from "react-icons/lu";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Mousewheel, EffectCoverflow } from "swiper/modules";
+import { FiChevronsRight } from "react-icons/fi";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+
+export default function GameDisplayTags({
+  tag = "multiplayer",
+  effect = "coverflow",
+}) {
+  const params = useParams();
+  const [display, setDisplay] = useState([]);
+
+  const getDisplay = async () => {
+    // const check = localStorage.getItem("grid")
+    // if(check){
+    // setGrid(JSON.parse(check))
+    // } else {
+    try {
+      const apiKey = import.meta.env.VITE_API_KEY;
+      const req = await fetch(
+        `https://api.rawg.io/api/games?key=${apiKey}&tags=${tag}&page_size=6`
+      );
+      if (!req.ok) {
+        throw new Error(`HTTP error! status: ${req.status}`);
+      }
+      const res = await req.json();
+      // localStorage.setItem("grid", JSON.stringify(res.results))
+      setDisplay(res.results);
+    } catch (error) {
+      console.error("Failed to fetch banner:", error);
+    }
+    // }
+  };
+
+  useEffect(() => {
+    getDisplay(params.content);
+  }, [params.content]);
+
+  return (
+    <div className="flex justify-center relative bg-background text-text">
+      <div className="w-full flex justify-center">
+        <div className="w-full gap-8 pt-12">
+          <Link
+            to={`/category/${tag}`}
+            className="text-4xl font-semi-bold flex items-end transition-all duration-500 hover:text-textLight"
+          >
+            {tag.charAt(0).toUpperCase() + tag.slice(1) + " " + "Games"}
+            <FiChevronsRight className="text-3xl" />
+          </Link>
+          <Swiper
+            className="text-center rounded-xl w-full pt-4"
+            slidesPerView={3}
+            pauseOnMouseEnter={true}
+            spaceBetween={25}
+            mousewheel={true}
+            effect={effect}
+            modules={[Mousewheel, EffectCoverflow]}
+          >
+            {display.map((items) => (
+              <SwiperSlide
+                key={items.id}
+                className="bg-backgroundLight shadow-xl rounded-xl transition-all duration-500 hover:bg-backgroundHover"
+              >
+                <Link to={`/game/${items.id}`}>
+                  <img
+                    className="p-4 rounded-xl"
+                    src={items.background_image}
+                    alt=""
+                    loading="lazy"
+                  />
+                  <div className="flex-col flex sm:flex-row justify-center gap-4 items-center pb-4">
+                    <h2 className="text-xl">{items.name}</h2>
+                    <LuBookmarkPlus className="text-2xl sm:text-3xl md:text-4xl text-primary transition-all duration-500 hover:text-secondary" />
+                  </div>
+                </Link>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  );
+}
