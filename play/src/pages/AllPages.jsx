7c812915346c21b53a028210b8499b1a16ef0b18
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-10-18 13:44:30 -0500

    Added Pages
    Added new pages with routes useable with useParams. Including a display page from buttons, list of game for genres, and actual game info page

--- play/src/pages/AllPages.jsx
+++ play/src/pages/AllPages.jsx
@@ -1,11 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Home from "./Home";
+import GameDisplay from "./GameDisplay";
+import Category from "./Category";
+import Game from "./Game";
+
 
 export default function AllPages() {
   return (
     <Routes>
       <Route path="/" element={<Home />}/>
+      <Route path="/main/:content" element={<GameDisplay />}/>
+      <Route path="/category/:type" element={<Category />}/>
+      <Route path="/game/:display" element={<Game />}/>
+      {/* <Route path="/main/:content" element={<GameDisplay />}/> */}
     </Routes>
   );
 }
