ab708c7edcbc4761f5eb056b6c480782895de87d
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-10-21 17:59:09 -0500

    Created gameinfo and gameinfoarray
    These will be used in my game file that displays the game the user wants to view with its information. I created separate components, because some of the results inside the game include strings, objects, and arrays. They are basically the same except for the infoarray one mapping through the information due to the result being an array. Also passed in certain styles to change depending on the element in the main document

---
+++ play/src/components/GameInfo.jsx
@@ -0,0 +1,19 @@
+export default function GameInfo({ title, items, span, size, color,padding }) {
+  return (
+    <div
+      className="bg-background bg-opacity-65 justify-center flex flex-col rounded-xl border-2 border-primary"
+      style={{ gridColumn: `span ${span}`, padding: `${padding}px`}}
+    >
+      <h3 className="text-2xl sm:text-3xl md:text-3xl lg:text-xl xl:text-3xl pb-4 text-secondary font-semibold">{title}</h3>
+      <div className="flex flex-wrap justify-center">
+        {items ? (
+          <div className="flex" style={{fontSize: `${size}px`, color: `${color}`}}>
+            <p className="font-normal">{items}</p>
+          </div>
+        ) : (
+          <p>No data available</p>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ play/src/components/GameInfoArray.jsx
@@ -0,0 +1,27 @@
+import { useParams } from "react-router-dom";
+import { LuBookmarkPlus } from "react-icons/lu";
+
+export default function GameInfoArray({ title, items, propertyName, span, padding }) {
+  return (
+    <div
+      className="bg-background bg-opacity-65 justify-center flex flex-col rounded-xl border-2 border-primary"
+      style={{ gridColumn: `span ${span}`, padding: `${padding}px`}}
+    >
+      <h3 className="text-2xl sm:text-3xl md:text-3xl lg:text-2xl xl:text-3xl pb-4 text-secondary font-semibold">{title}</h3>
+      <div className="flex gap-1 flex-wrap justify-center lg:pb-0 text-sm sm:text-base  lg:xl">
+        {items && items.length > 0 ? (
+          items.map((item, index) => (
+            <div className="flex" key={index}>
+              <p className="font-normal">
+                {item[propertyName]}
+                {index < items.length - 1 && <span>,</span>}
+              </p>
+            </div>
+          ))
+        ) : (
+          <p>No data available</p>
+        )}
+      </div>
+    </div>
+  );
+}
