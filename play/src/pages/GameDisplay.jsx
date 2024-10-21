2d042eea6f799209c347e1719205b6ffd73c5b69
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-10-21 18:05:16 -0500

    Created gamedisplay
    This page displays a variants of games. It uses the gamedisplaytags and bodyscroll components with just changing styles passed through.

---
+++ play/src/pages/GameDisplay.jsx
@@ -0,0 +1,33 @@
+import GameDisplayTags from "../components/GameDisplayTags";
+import BodyScroll from "../components/HomePage/BodyScroll";
+import { GiArrowCursor } from "react-icons/gi";
+
+export default function GameDisplay() {
+  return (
+    <div className="flex justify-center relative text-text bg-background">
+      <div
+        className="w-full flex justify-center"
+        style={{
+          background: "linear-gradient(to bottom, #D04D11 0%,#070C0D 10%)",
+        }}
+      >
+        <div className="w-9/12 flex flex-col gap-8 pt-24 pb-12">
+          <div className="flex justify-center items-end gap-4">
+            <h1 className="font-bold text-6xl lg:text-7xl xl:text-8xl text-center">
+              Games To Play
+            </h1>
+            {/* <GiArrowCursor className="font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl" /> */}
+          </div>
+          <GameDisplayTags />
+          <div className="grid grid-cols-1 lg:grid-cols-2 items-center">
+            <BodyScroll mousewheel="false" speed="1500" genres="3" />
+            <BodyScroll mousewheel="false" genres="5" />
+          </div>
+          <GameDisplayTags effect="none" tag="survival" />
+          <GameDisplayTags effect="none" tag="action-adventure" />
+          <GameDisplayTags effect="none" tag="split-screen" />
+        </div>
+      </div>
+    </div>
+  );
+}
