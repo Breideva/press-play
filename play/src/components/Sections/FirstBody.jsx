283f1004102ba5902a09506a6d0cfeb791fe4272
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-10-25 14:05:28 -0500

    Changed game results

--- play/src/components/Sections/FirstBody.jsx
+++ play/src/components/Sections/FirstBody.jsx
@@ -8,8 +8,8 @@ export default function FirstBody() {
     >
       <div className="w-full flex justify-center">
         <div className="w-9/12 flex flex-col lg:flex-row gap-y-10 lg:gap-8 justify-center items-center pt-14 mb-20 overflow-hidden">
-          <BigBanner />
-          <BigBanner genres="3" />
+          <BigBanner name="22509" />
+          <BigBanner name="23598"/>
         </div>
       </div>
     </div>
