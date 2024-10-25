37a91f1d379fefe1f189cbf4f86ff06c4c4e1ffe
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-10-25 14:06:40 -0500

    Changed reuslts
    Instead of grabbing genres and using just one I simply changed it to only grab one game making both of the banners popular games

--- play/src/components/HomePage/BigBanner.jsx
+++ play/src/components/HomePage/BigBanner.jsx
@@ -3,28 +3,28 @@ import { FaStar } from "react-icons/fa";
 import { LuBookmarkPlus } from "react-icons/lu";
 import { Link } from "react-router-dom";
 
-export default function BigBanner({ genres = 6 }) {
-  const [banner, setBanner] = useState([]);
+export default function BigBanner({ name }) {
+  const [banner, setBanner] = useState({});
+  const [screenshot, setScreenshot] = useState([])
 
   const getBanner = async () => {
-    // const check = localStorage.getItem("banner-one");
-    // if (check) {
-    //   setBanner(JSON.parse(check));
-    // } else {
     try {
       const apiKey = import.meta.env.VITE_API_KEY;
       const req = await fetch(
-        `https://api.rawg.io/api/games?key=${apiKey}&genres=${genres}&page_size=1`
+        `https://api.rawg.io/api/games/${name}?key=${apiKey}`
+      );
+      const ssReq = await fetch(
+        `https://api.rawg.io/api/games/${name}/screenshots?key=${apiKey}`
       );
       if (!req.ok) {
         throw new Error(`HTTP error! status: ${req.status}`);
       }
+      const ssRes = await ssReq.json()
       const res = await req.json();
-      // localStorage.setItem("banner-one", JSON.stringify(res.results));
-      setBanner(res.results);
+      setBanner(res);
+      setScreenshot(ssRes.results);
     } catch (error) {
       console.error("Failed to fetch banner:", error);
-      // }
     }
   };
 
@@ -32,29 +32,28 @@ export default function BigBanner({ genres = 6 }) {
     getBanner();
   }, []);
   useEffect(() => {
-    // console.log(banner[0].id)
+    console.log(screenshot);
   }, [banner]);
 
   return (
     <div className="w-11/12 lg:w-5/12 z-20 pb-8">
-      {banner.map((items) => (
         <Link
-        to={`/game/${items.id}`}
-          key={items.id}
+        to={`/game/${banner.id}`}
+          key={banner.id}
           className="bg-backgroundLight w-full block rounded-xl p-8 transition-transform duration-700 shadow-xl hover:bg-backgroundHover hover:scale-105"
         >
           <img
             className="w-fit rounded-xl"
-            src={items.background_image}
+            src={banner.background_image}
             loading="lazy"
             alt=""
           />
           <div className="flex gap-2 my-2">
-            {items.short_screenshots.map((item) => (
-              <div key={item.id} className="flex">
+            {screenshot.map((items) => (
+              <div key={items.id} className="flex">
                 <img
                   className="hover:scale-105 transition-transform duration-300 h-fit"
-                  src={item.image}
+                  src={items.image}
                   loading="lazy"
                   alt=""
                 />
@@ -63,7 +62,7 @@ export default function BigBanner({ genres = 6 }) {
           </div>
           <div className="flex gap-4 items-center justify-center my-4">
             <h2 className="text-xl sm:text-3xl md:4xl xl:text-5xl font-bold">
-              {items.name}
+              {banner.name}
             </h2>
 
             <LuBookmarkPlus className="text-2xl sm:text-3xl md:text-4xl text-primary transition-all duration-500 hover:text-secondary" />
@@ -73,20 +72,20 @@ export default function BigBanner({ genres = 6 }) {
               <span className="text-lg sm:text-2xl md:text-3xl font-semibold">
                 Released:
               </span>{" "}
-              {items.released}
+              {banner.released}
             </p>
             <p className="flex items-center">
               <span className="text-lg sm:text-2xl md:text-3xl font-semibold mr-2">
                 Rating:
               </span>{" "}
-              {items.rating}
-              <FaStar className="text-yellow-500" />({items.reviews_count})
+              {banner.rating}
+              <FaStar className="text-yellow-500" />({banner.reviews_count})
             </p>
             <p>
               <span className="text-lg sm:text-2xl md:text-3xl font-semibold">
                 Downloads:
               </span>{" "}
-              {items.added.toLocaleString()}
+              {banner?.added?.toLocaleString()}
             </p>
             <div className="flex flex-wrap gap-1 items-end">
               <p className="mr-1">
@@ -94,16 +93,16 @@ export default function BigBanner({ genres = 6 }) {
                   Genres:
                 </span>
               </p>
-              {items.genres.map((item, index) => (
+              {banner?.genres?.map((item, index) => (
                 <div key={item.id} className="flex">
-                  <p>{item.name}{index < items.genres.length - 1 && ", "}</p>
+                  <p>{item.name}{index < banner.genres.length - 1 && ", "}</p>
                   
                 </div>
               ))}
             </div>
           </div>
         </Link>
-      ))}
+      {/* ))} */}
     </div>
   );
 }
