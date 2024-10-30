97acf2c2d420ea1b75725acf8c91be301fbfe457
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-10-30 13:43:42 -0500

    Added game, actualGame, and result
    The game state will be used for areas like the gameGrid for calling the game. The getGame function gets the games from rawg. Actual game and result work together for the profile section. getGameId is used to grab the id of the games' bookmark that's clicked. With that click it also sets the result that's then passed into getActualGame which grabs the game using the id

--- play/src/context/ProfileContext.jsx
+++ play/src/context/ProfileContext.jsx
@@ -3,11 +3,59 @@ import { createContext } from "react";
 
 export const Context = createContext(null);
 
-
 export const GamesContext = (props) => {
+  const [game, setGame] = useState([]);
+  const [size, setSize] = useState(8);
+  const [result, setResult] = useState(null);
+  const [actualGame, setActualGame] = useState({});
 
 
-  const contextValue = {};
+  const getGame = async (name) => {
+    try {
+      const apiKey = import.meta.env.VITE_API_KEY;
+      const req = await fetch(
+        `https://api.rawg.io/api/games?key=${apiKey}&genres=${name}&page_size=${size}`
+      );
+      if (!req.ok) {
+        throw new Error(`HTTP error! status: ${req.status}`);
+      }
+      const res = await req.json();
+      setGame(res.results);
+    } catch (error) {
+      console.error("Failed to fetch banner:", error);
+      // }
+    }
+  };
+  
+  const getGameId = (game) => {
+    return game?.id;
+  };
+
+  const getActualGame = async (id) => {
+    try {
+      const apiKey = import.meta.env.VITE_API_KEY;
+      const req = await fetch(
+        `https://api.rawg.io/api/games/${id}?key=${apiKey}`
+      );
+  
+      if (!req.ok) {
+        throw new Error(`HTTP error! status: ${req.status}`);
+      }
+      const res = await req.json();
+      setActualGame(res);
+    } catch (error) {
+      console.error("Failed to fetch Game:", error);
+    }
+  };
+  
+  useEffect(() => {
+    if (result) {
+      getActualGame(result);
+    }
+  }, [result]);
+  
+
+  const contextValue = { game, setGame, getGame, getGameId };
 
   return (
     <Context.Provider value={contextValue}>{props.children}</Context.Provider>
