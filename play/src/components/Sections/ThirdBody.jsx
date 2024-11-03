c478a4c6c1b7b5e7ae03841cae75fea50e8d1be3
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-11-03 15:14:06 -0600

    Changed Genre

--- play/src/components/Sections/ThirdBody.jsx
+++ play/src/components/Sections/ThirdBody.jsx
@@ -6,8 +6,8 @@ export default function ThirdBody() {
       <div className="w-full flex justify-center">
         <div className="w-9/12 flex flex-col lg:flex-row gap-y-10 gap:0 justify-center items-center">
           <div className="grid grid-cols-1 lg:grid-cols-2 w-full justify-center items-center">
-            <BodyScroll />
-             <BodyScroll genres="15" speed="1500" />
+            <BodyScroll genres="11" speed="750" />
+            <BodyScroll genres="3" speed="1500" />
           </div>
         </div>
       </div>
