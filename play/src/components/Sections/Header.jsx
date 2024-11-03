d641d2b31fb5b31319ba74993e667367f2ddf393
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-11-03 15:11:53 -0600

    Changed Header to Sections folder

--- play/src/components/Sections/Header.jsx
+++ play/src/components/Sections/Header.jsx
@@ -7,13 +7,13 @@ export default function Header() {
   return (
     <div className="flex justify-center bg-background">
       <div className="w-full flex justify-center">
-        <div className="w-9/12 justify-center flex flex-col sm:flex-row gap-14 sm:gap-0 items-center mt-20 sm:mt-30 md:mt-40 mb-20 sm:mb-30 md:mb-40 ">
+        <div className="w-9/12 justify-center flex flex-col sm:flex-row gap-14 sm:gap-0 items-center mt-20 sm:mt-30 xl:mt-20 md:mt-40 mb-20 sm:mb-30 md:mb-40 ">
           <div className="relative w-3/4 sm:w-1/2">
             <img className="absolute z-20" src={person} alt="" />
             <img className="z-10 relative" src={circles} alt="" />
           </div>
           <div className="w-full sm:w-1/2 flex flex-col justify-center gap-y-6 sm:gap-y-12 items-center text-center font-semibold relative">
-            <h1 className="text-text text-4xl md:text-5xl lg:text-6xl xl:text-8xl relative z-20 font-bold">
+            <h1 className="text-text text-4xl md:text-5xl lg:text-6xl xl:text-7xl relative z-20 font-bold">
               Discover and save your favorite games in one place.
             </h1>
             <div className="flex items-center gap-8 sm:gap-6 md:gap-10 lg:gap-14">
--- play/src/pages/Home.jsx
+++ play/src/pages/Home.jsx
@@ -1,6 +1,5 @@
-import BigBanner from "../components/HomePage/BigBanner";
 import FirstBody from "../components/Sections/FirstBody";
-import Header from "../components/HomePage/Header";
+import Header from "../components/Sections/Header";
 import SecondBody from "../components/Sections/SecondBody";
 import GameGrid from "../components/HomePage/GameGrid";
 import ThirdBody from "../components/Sections/ThirdBody";
