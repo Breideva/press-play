91f9e9ec7b626dcb4921163830082fdc2fc585aa
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-12-20 14:40:00 -0600

    Updated AllPages
    Wrapped everything in StartAtTop so that every time the location changes it always scrolls to the top of the window.

--- play/src/pages/AllPages.jsx
+++ play/src/pages/AllPages.jsx
@@ -1,21 +1,24 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "../components/Navbar";
 import Home from "./Home";
 import GameDisplay from "./GameDisplay";
 import Category from "./Category";
 import Game from "./Game";
 import Searched from "./Searched";
-
+import Profile from "./Profile";
+import { StartAtTop } from "../context/ProfileContext";
 
 export default function AllPages() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/main/:content" element={<GameDisplay />}/>
-      <Route path="/category/:type" element={<Category />}/>
-      <Route path="/game/:display" element={<Game />}/>
-      <Route path="/search/:menu" element={<Searched />}/>
-      {/* <Route path="/main/:content" element={<GameDisplay />}/> */}
-    </Routes>
+    <>
+      <StartAtTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/main/:content" element={<GameDisplay />} />
+        <Route path="/category/:type" element={<Category />} />
+        <Route path="/game/:display" element={<Game />} />
+        <Route path="/search/:menu" element={<Searched />} />
+        <Route path="/profile/:select" element={<Profile />} />
+      </Routes>
+    </>
   );
 }
