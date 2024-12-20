a1dab0389c3be89c1538974c17e32f7e509153df
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-12-20 14:37:40 -0600

    Updated Category styles

--- play/src/pages/Category.jsx
+++ play/src/pages/Category.jsx
@@ -43,37 +43,20 @@ export default function Category() {
 
   return (
     <div className="flex justify-center relative min-h-screen bg-background text-text">
-      <div className="w-full flex justify-center">
-        <div className="w-9/12 flex flex-col text-center gap-8 pt-24 pb-12">
-          <h1 className="text-4xl lg:text-5xl xl:text-7xl font-bold pt-12 pb-4">
+      <div className="w-full flex flex-col items-center justify-center">
+        <div className="w-full bg-backgroundHover flex flex-col items-center justify-center">
+          <h1 className="text-4xl lg:text-5xl xl:text-7xl font-bold mt-12 py-8">
             {params.type.charAt().toUpperCase() +
               params.type.slice(1) +
               " " +
               "Games"}
           </h1>
-          <div className="text-text gap-4 justify-center items-center text-center w-full">
+          <div className="h-12 bg-backgroundLight w-full"></div>
+        </div>
+        <div className="flex flex-col text-center gap-8 pb-12">
+          <div className="text-text gap-4 flex justify-center items-start text-center w-full h-full ">
             <GameGrid tag={params.type} title="" />
-            {/* {category.map((items) => (
-              <Link to={`/game/${items.id}`} key={items.id}>
-                <div className="bg-backgroundLight  shadow-xl rounded-xl transition-all duration-500 hover:bg-backgroundHover hover:scale-105">
-                  <img
-                    className="p-4 rounded-xl"
-                    src={items.background_image}
-                    alt=""
-                  />
-                  <div className="flex-col flex sm:flex-row justify-center gap-4 items-center pb-4">
-                    <h2 className="text-xl">{items.name}</h2>
-                    <LuBookmarkPlus className="text-2xl sm:text-3xl md:text-4xl text-primary transition-all duration-500 hover:text-secondary" />
-                  </div>
-                </div>
-              </Link>
-            ))} */}
           </div>
-          {/* <div>
-            <button onClick={addSize} className="p-4 bg-backgroundLight rounded-xl shadow-xl transition-all duration-500 hover:scale-105 hover:bg-backgroundHover">
-              {button}
-            </button>
-          </div> */}
         </div>
       </div>
     </div>
