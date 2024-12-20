8bdfadb1d24c85b1c49290999601c2a73d1f68a5
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-12-20 14:32:29 -0600

    Updated config

--- play/vite.config.js
+++ play/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        // All requests that start with '/api' will be proxied
+        target: "https://api.rawg.io", // The target API server to forward requests to
+        changeOrigin: true, // Changes the origin of the host header to the target URL
+        rewrite: (path) => path.replace(/^\/api/, ""), // Removes '/api' from the request path
+      },
+    },
+  },
+});
