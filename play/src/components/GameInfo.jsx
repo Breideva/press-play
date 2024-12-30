241498e5edfb78e08a44be3c3243e1996b6f0149
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-12-30 15:37:41 -0600

    Fixed overflow issue

--- play/src/components/GameInfo.jsx
+++ play/src/components/GameInfo.jsx
@@ -1,7 +1,7 @@
 export default function GameInfo({ title, items, span, size, color, padding }) {
   return (
     <div
-      className="bg-background bg-opacity-65 justify-center flex flex-col rounded-xl border-2 border-primary"
+      className="bg-background bg-opacity-65 justify-center flex flex-col rounded-xl border-2 border-primary overflow-hidden"
       style={{ gridColumn: `span ${span}`, padding: `${padding}px` }}
     >
       <h3 className="text-2xl sm:text-3xl md:text-3xl lg:text-xl xl:text-3xl pb-4 text-secondary font-semibold">
--- play/src/components/GameInfoArray.jsx
+++ play/src/components/GameInfoArray.jsx
@@ -1,7 +1,7 @@
 export default function GameInfoArray({ title, items, propertyName, span, padding }) {
   return (
     <div
-      className="bg-background bg-opacity-65 justify-center flex flex-col rounded-xl border-2 border-primary"
+      className="bg-background bg-opacity-65 justify-center flex flex-col rounded-xl border-2 border-primary overflow-hidden"
       style={{ gridColumn: `span ${span}`, padding: `${padding}px`}}
     >
       <h3 className="text-2xl sm:text-3xl md:text-3xl lg:text-2xl xl:text-3xl pb-4 text-secondary font-semibold">{title}</h3>
@@ -9,7 +9,7 @@ export default function GameInfoArray({ title, items, propertyName, span, paddin
         {items && items.length > 0 ? (
           items.map((item, index) => (
             <div className="flex" key={index}>
-              <p className="font-normal">
+              <p className="text-sm sm:text-base font-normal">
                 {item[propertyName]}
                 {index < items.length - 1 && <span>,</span>}
               </p>
